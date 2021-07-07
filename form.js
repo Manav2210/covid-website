3d7f05771a177935120c37178b6f1e3e3d9b4bbe
Manav2210 <86183290+Manav2210@users.noreply.github.com>
2021-07-07 16:58:29 +0530

    Add files via upload

---
+++ form.js
@@ -0,0 +1,90 @@
+// Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyAIgQX-XEGLuKcOUzF2A5U7qRVJ_T2rSpo",
+    authDomain: "corona-project-6c0c4.firebaseapp.com",
+    databaseURL: "https://corona-project-6c0c4-default-rtdb.firebaseio.com",
+    projectId: "corona-project-6c0c4",
+    storageBucket: "corona-project-6c0c4.appspot.com",
+    messagingSenderId: "214319414397",
+    appId: "1:214319414397:web:25c80e3388b37e6b32c514"
+  };
+  firebase.initializeApp(firebaseConfig);
+
+ /* Validation of data collected through form,
+  on click event of Submit button, submitForm function is called */
+  var UserInputsRef=firebase.database().ref('UserInputs')
+  document.getElementById('testForm').addEventListener('submit',submitForm);
+
+  /* function store input values in variables */
+  function submitForm(e){
+    e.preventDefault();
+    var fname =getInputVal('firstname');
+    var lname =getInputVal('lastname');
+    var mobile =getInputVal('mobile');
+    var state =getInputVal('state');
+    state=state.toLowerCase();
+    readState(state);
+    var email =getInputVal('email');
+    var emailstatus=validateEmail();
+    var profession =getInputVal('profession');
+    var dateofbirth =getInputVal('dateofbirth');
+    var symptomsList =getSelectedCheckboxValues('symptoms');
+    var selectedOption = document.querySelector('input[name = option]:checked').value;
+    /* function call to store data values in firebase
+    after email id validation  */
+    if(emailstatus)
+    saveMessages(lname+ " " +fname,mobile,email,profession,dateofbirth,state,selectedOption,symptomsList);
+}
+
+/* function to accept state value as parameter, read database
+ to return and display center details on web page */
+function readState(state){
+    var centers;
+    var ref = firebase.database().ref(state);
+    ref.on('value', (data) => {
+     centers = data.val();
+     document.getElementById("result").innerHTML ="<br>"+centers.toUpperCase();
+    })
+}
+/* function to return input values as per the id passed as parameter */
+function getInputVal(id){
+    return document.getElementById(id).value;
+}
+
+/* function to write collected details in firebase,
+create new record and add values in respective fields */
+function saveMessages(name,mobile,email,profession,dateofbirth,state,selectedOption,symptomsList){
+    var newuserInputsRef = UserInputsRef.push();
+    newuserInputsRef.set({
+        name:name,
+        mobile:mobile,
+        email:email,
+        profession:profession,
+        dateofbirth:dateofbirth,
+        selectedOption:selectedOption,
+        state:state,
+        symptomsList:symptomsList
+    })
+    alert("Thank you, find the list of centers nearby!  ");
+}
+
+/* function to return value(s) of selcted checkboxes */
+function getSelectedCheckboxValues(name) {
+    const checkboxes = document.querySelectorAll(`input[name="${name}"]:checked`);
+    let values = [];
+    checkboxes.forEach((checkbox) => {
+        values.push(checkbox.value);
+    });
+    return values;
+}
+
+/* function to check if email id entered by user is valid */
+function validateEmail()
+{
+ if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(testForm.email.value))
+  {
+    return (true)
+  }
+    alert("You have entered an invalid email address!")
+    return (false)
+}
